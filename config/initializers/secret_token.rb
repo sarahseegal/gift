1e4eb2594d8e44e4a057099d5a4543d605e80321
Sarah Seegal <sarahseegal@unknownd8a25e928261.gateway.2wire.net>
2011-11-30 13:19:08 -0800

    basic app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gift::Application.config.secret_token = 'd256fe77e87eb928d158b85c542539758874d3532ea059f750ae0d36e3936428516691f7d33fde7fe608fa1ee04a4198463ad823c9432dbafeff2a4dca4f1696'
